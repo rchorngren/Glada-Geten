436cff9e177524a6aa61cf505bdc196f00fba800
skabobo <skabobo@hotmail.com>
2016-12-13 13:59:37 +0100

    Revert "html filer tll php"
    This reverts commit 221f4ed61a424a48e1d89413bc56c491ef656446.

--- login/profile.php
+++ login/profile.php
@@ -1,8 +1,7 @@
+
 <?php
 include('session.php');
-include('textadmin.php');
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,40 +18,8 @@ include('textadmin.php');
 		<li><a href="profile.php" class="current">Text admin</a></li>
 		<li><a href="image.php">Bild admin</a></li>
 		<li><a href="booking.php">Bokningar</a></li>			
-	</ul>	
+	</ul>
+	
 </nav>
-
-<?php
-
-$query = "SELECT * FROM pages";
-$pages_result = mysqli_query($db, $query);
-
-while($page = mysqli_fetch_assoc($pages_result)){
-	echo '
-		<div class="text-edit">
-			<h1 class="text-heading">'.$page['page_name'].'</h1>
-			<form class="edit" method="POST">
-				<textarea name="formtext1" rows="20" cols="80">'.$page['page_content1'].'</textarea>
-				<textarea name="formtext2" rows="20" cols="80">'.$page['page_content2'].'</textarea>
-				<textarea name="formtext3" rows="20" cols="80">'.$page['page_content3'].'</textarea>
-				<input type="hidden" name="id" value="'.$page['id'].'">
-			    <button type="submit" name="update">Spara</button>
-			</form>
-		</div>
-	';
-}
-
-?>
-
-<script type="text/javascript">
-	var ta = document.getElementsByTagName("textarea");
-	for (var i = 0; i < ta.length; i++) {
-		if (ta[i].value == "") {
-			ta[i].style.display = "none";
-		};
-	}
-</script>
-
-
 </body>
 </html>
--- login/session.php
+++ login/session.php
@@ -1,19 +1,18 @@
 
 <?php
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysqli_connect("localhost", "root", "", "company");
+$connection = mysql_connect("localhost", "root", "");
 // Selecting Database
-//$db = mysql_select_db("company", $connection);
+$db = mysql_select_db("company", $connection);
 session_start();// Starting Session
 // Storing Session
 $user_check=$_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-$query = "select username from login2 where username='$user_check'";
-$ses_sql=mysqli_query($connection, $query);
-$row = mysqli_fetch_assoc($ses_sql);
+$ses_sql=mysql_query("select username from login2 where username='$user_check'", $connection);
+$row = mysql_fetch_assoc($ses_sql);
 $login_session =$row['username'];
 if(!isset($login_session)){
-mysqli_close($connection); // Closing Connection
+mysql_close($connection); // Closing Connection
 header('Location: index.php'); // Redirecting To Home Page
 }
 ?>

bf721309153005c1315a356191c94ea3d0d39c9a
Robert Horngren <robert.horngren@student.kyh.se>
2016-12-13 14:29:52 +0100

    Corrected database login
    Now corresponds to live server

--- login/login.php
+++ login/login.php
@@ -11,7 +11,7 @@ else
 $username=$_POST['username'];
 $password=$_POST['password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysqli_connect("localhost", "root", "", "company");
+$connection = mysqli_connect("gg-219291.mysql.binero.se", "219291_ow20538", "Sommar16", "219291-gg");
 // To protect MySQL injection for Security purpose
 //$username = stripslashes($username);
 //$password = stripslashes($password);
@@ -20,7 +20,7 @@ $connection = mysqli_connect("localhost", "root", "", "company");
 // Selecting Database
 //$db = mysql_select_db("company", $connection);
 // SQL query to fetch information of registerd users and finds user match.
-$query = "select * from login2 where password='$password' AND username='$username'";
+$query = "select * from login where password='$password' AND username='$username'";
 $rows = mysqli_query($connection, $query);
 //$rows = mysqli_num_rows($query);
 if ($rows == 1) {

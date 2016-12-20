16b666097897d645aa6fabeea7cf5f026de210f9
skabobo <skabobo@hotmail.com>
2016-12-20 15:38:15 +0100

    bookingadmin

--- login/booking.php
+++ login/booking.php
@@ -1,5 +1,7 @@
 <?php
 include('session.php');
+$db = mysqli_connect("localhost", "root", "", "company");
+mysqli_query($db, "SET NAMES utf8");
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,7 +20,42 @@ include('session.php');
 		<li><a href="image.php">Bild admin</a></li>
 		<li><a href="booking.php" class="current">Bokningar</a></li>			
 	</ul>
-	
 </nav>
+<div class="booking-edit">
+<?php 
+
+$query = "SELECT * FROM bookings ORDER BY arrives ASC"; 
+$booking_result = mysqli_query($db, $query);
+
+
+if ($booking_result->num_rows > 0) {
+     // output data of each row
+    while ($row = $booking_result->fetch_assoc()) {
+        echo 
+        "<div class='inside'>"
+        . $row["id"].
+        ". </br><p class='booking-text'> Datum:</p>". $row["date"]. 
+        " </br><p class='booking-text'>Namn:</p> ".$row["firstName"]. 
+        " </br><p class='booking-text'>Efternamn:</p>  ".$row["lastName"]. 
+        " </br><p class='booking-text'>Email:</p>  ".$row["email"]. 
+        " </br><p class='booking-text'>Tel:</p>  ".$row["phone"]. 
+        " </br><p class='booking-text'>Adress:</p>  ".$row["address"]. 
+        " </br><p class='booking-text'>Postnr:</p>  ".$row["zipCode"]. 
+        " </br><p class='booking-text'>Stad:</p>  ".$row["city"]. 
+        " </br><p class='booking-text'>Land:</p>  ".$row["country"]. 
+        " </br><p class='booking-text'>Ankomst:</p>  ".$row["arrives"]. 
+        " </br><p class='booking-text'>Utcheck:</p>  ".$row["departs"]. 
+        " </br><p class='booking-text'>Rum:</p>  ".$row["room"]. 
+        " </br><p class='booking-text'>Aktivitet:</p>  ".$row["activity"]. 
+        " </br><p class='booking-text'>Info:</p>  ".$row["information"]. 
+        "</div>";
+    }
+} else {
+     echo "0 results";
+}
+
+?>
+		
+</div>
 </body>
 </html>
--- login/login.php
+++ login/login.php
@@ -11,9 +11,10 @@ else
 $username=$_POST['username'];
 $password=$_POST['password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysqli_connect('gg-219291.mysql.binero.se', '219291_ow20538', 'Sommar16', '219291-gg');
+//$connection = mysqli_connect('gg-219291.mysql.binero.se', '219291_ow20538', 'Sommar16', '219291-gg');
+$connection = mysqli_connect("localhost", "root", "", "company");
 // SQL query to fetch information of registerd users and finds user match.
-$query = "select * from login where password='$password' AND username='$username'";
+$query = "select * from login2 where password='$password' AND username='$username'";
 $rows = mysqli_query($connection, $query);
 if ($rows == 1) {
 $_SESSION['login_user']=$username; // Initializing Session
